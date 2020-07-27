29ec25766a4abe69227dd49326bca0a19a730575
Vikram <vikrambalaji2k@gmail.com>
2020-07-27 19:40:29 +0530

    Added CORSApi

---
+++ week2/task1/CORSApi/scripts.js
@@ -0,0 +1,14 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'http://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com', true)
+request.setRequestHeader('Access-Control-Allow-Origin', '*');
+request.onload = function() {
+    // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  console.log(data)
+  }
+  
+  // Send request
+  request.send()
