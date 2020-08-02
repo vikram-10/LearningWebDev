b5136b41ae8bbcd35623957a0d73489c083434e5
Vikram Balaji <vikrambalaji2k@gmail.com>
2020-08-02 18:11:07 +0530

    Added OOPS folder

---
+++ week2/task5/oopstasks/oops1.js
@@ -0,0 +1,10 @@
+class Movie{
+    constructor(title,studio,rating){
+        this.title=title;
+        this.studio=studio;
+        this.rating=rating;
+    }
+    getPG(){
+           //return movies with PG rating
+    }
+}
