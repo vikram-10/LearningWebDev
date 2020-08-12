dc4608b618d06f1093c4829d5d0906dc34e56fdc
Vikram Balaji <vikrambalaji2k@gmail.com>
2020-08-12 10:21:53 +0530

    Addded retouched date task

--- week4/task1/datemanip/index.js
+++ week4/task1/datemanip/index.js
@@ -11,6 +11,8 @@ var ipLabel=document.createElement('label');
 ipLabel.setAttribute('for','dob');
 ipLabel.innerText="Enter DOB:";
 
+var breaker=document.createElement('br');
+
 var ele=document.createElement('input');
 ele.setAttribute('type','date');
 ele.setAttribute('required','true');
@@ -21,22 +23,46 @@ ele.setAttribute('id','dateip')
 var submitButton=document.createElement('button');
 submitButton.innerText="Submit";
 
+var op1=document.createElement('div');
+op1.setAttribute('class','output');
+op1.setAttribute('id','oped');
+op1.innerText="Year Difference:";
+
+var op2=document.createElement('div');
+op2.setAttribute('class','output');
+op2.innerText="Month Difference:";
+
+var op3=document.createElement('div');
+op3.setAttribute('class','output');
+op3.innerText="Day Difference:";
+
 document.body.append(header);
 document.body.append(inner);
 inner.append(form);
 form.append(ipLabel);
+form.append(breaker);
 form.append(ele);
 form.append(submitButton);
 
 
 submitButton.addEventListener('click',function(){
-    var ip=document.getElementById('dateip').value;
-    console.log(ip);
-    let now = new Date();
- 
-    let day = ("0" + now.getDate()).slice(-2);
-    let month = ("0" + (now.getMonth() + 1)).slice(-2);
-
-    let today = (day)+"-"+(month)+"-"+ now.getFullYear();
-    console.log(today);
-})
+    event.preventDefault();
+    var date= ele.value;
+    var splitDate=date.split("-");
+    var ipYear=splitDate[0];
+    var ipMonth=splitDate[1];
+    var ipDay=splitDate[2];
+    y=new Date();
+    var day=y.getDate();
+    var month=y.getMonth()+1;
+    var year=y.getFullYear();
+    var yearDifference=year-ipYear;
+    var monthDifference=month-ipMonth;
+    var dayDifference=day-ipDay;
+    inner.append(op1);
+    op1.innerText+=yearDifference;
+    inner.append(op2);
+    op2.innerText+=monthDifference;
+    inner.append(op3);
+    op3.innerText+=dayDifference;
+});
