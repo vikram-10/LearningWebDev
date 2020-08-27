d4aa80ef6bb29dbe4ad08f9a94b828b3ed1fc9f0
Vikram Balaji <vikrambalaji2k@gmail.com>
2020-08-27 20:55:32 +0530

    Removed practice class

--- week6/task4/final.ts
+++ week6/task4/final.ts
@@ -1,21 +1,7 @@
-var unirest = require("unirest");
+async function nibba(){
+var fetcher=await fetch(`https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?rapidapi-key=20363ac113mshb31c281692decb1p1a7cccjsn1d7b3f349556&locale=en-US&id=40008598`);
+var fetcherJson=await fetcher.json();
+console.log(fetcherJson);
+}
 
-var req = unirest("GET", "https://shazam.p.rapidapi.com/songs/list-recommendations");
-
-req.query({
-	"locale": "en-US",
-	"key": "484129036"
-});
-
-req.headers({
-	"x-rapidapi-host": "shazam.p.rapidapi.com",
-	"x-rapidapi-key": "20363ac113mshb31c281692decb1p1a7cccjsn1d7b3f349556",
-	"useQueryString": true
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+nibba();
