4da66de87eb0fdd29eec6f24a9febb7b93054ec2
Vikram Balaji <vikrambalaji2k@gmail.com>
2020-08-27 16:33:47 +0530

    Added some tv info

--- week6/task3/tv.ts
+++ week6/task3/tv.ts
@@ -1,3 +1,6 @@
-document.getElementById('upButton').addEventListener('click',function(){
-    alert("Up Button Clicked!");
-})
+document.getElementById('upButton').addEventListener('click', function () {
+    var videoDiv=document.createElement('div');
+    videoDiv.setAttribute('style','display:inline')
+    videoDiv.innerHTML=`<iframe width="560" height="315" src="https://www.youtube.com/embed/HfTXHrWMGVY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    document.body.append(videoDiv);
+});
